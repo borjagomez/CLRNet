e08874ef0ee9b23b9667c1bfef7e4df357e42b42
Borja Gómez Muñoz <borja@reverscore.com>
2022-10-19 15:59:49 +0200

    Modify setup

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
+echo "First create google.json in the root directory. Press Enter to confirm."
+read content
+
 pip install dvc
 pip install 'dvc[gs]'
 pip install torch==1.8.0 torchvision==0.9.0
 
 python setup.py build develop
 
-echo "Create google.json in the root directory:"
-read content
-echo "$content" >> ./google.json
-
+dvc remote modify --local gcp credentialpath 'google.json'
 dvc pull
